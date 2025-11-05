12f99ee35a9d234a08807aba4533d0c3ec9abb03
Medisa Aris Ginanjar <medisa.aris@eris.co.id>
2025-11-05 09:00:57 +0700

    update for pwa

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,35 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pwa: {
+    name: 'Technoshop',
+    themeColor: '#42b983',
+    msTileColor: '#000000',
+    manifestOptions: {
+      short_name: 'TCS',
+      start_url: '.',
+      display: 'standalone',
+      background_color: '#ffffff'
+    },
+    workboxPluginMode: 'GenerateSW', // keep GenerateSW for automatic service worker
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
+      runtimeCaching: [
+        {
+          // Match API requests to https://fakestoreapi.com/products
+          urlPattern: /^https:\/\/fakestoreapi\.com\/products/,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'products-cache',
+            expiration: {
+              maxEntries: 50,        // max 50 products cached
+              maxAgeSeconds: 24 * 60 * 60, // 1 day
+            },
+            cacheableResponse: {
+              statuses: [0, 200], // cache successful responses
+            },
+          },
+        },
+      ],
+    },
+  },
+};
